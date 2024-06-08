21031007cff47b9bef33ee41aaf2adf78b6dd546
phan <thisisphan@gmail.com>
2024-06-08 19:27:28 -0400

    vscode api bad :( should probably just use tsserver api

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,9 @@
 import * as vsc from "vscode";
-import { initAddMissingImportsCMD } from "./commands/add-missing-imports";
 import { initDevCMD } from "./commands/dev";
 import { initDifSelectionWithClipboardCMD } from "./commands/diff-selection-with-clipboard";
 
 export function activate(context: vsc.ExtensionContext) {
   initDifSelectionWithClipboardCMD(context);
-  initAddMissingImportsCMD(context);
   initDevCMD(context);
 }
 
